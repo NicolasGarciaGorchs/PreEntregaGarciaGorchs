de27679052d915b1467a7e1d3ec1e343e9c0b7a7
Nicolas Garcia Gorchs <nicolasgarciagorchs@gmail.com>
2023-06-21 00:46:41 -0300

    Segunnda

--- PreEntregaGarciaGorchs/src/components/Repuestos/Repuestos.jsx
+++ PreEntregaGarciaGorchs/src/components/Repuestos/Repuestos.jsx
@@ -3,14 +3,12 @@ import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import ItemListContainer from './ItemListContainer/ItemListContainer'
 import ContenidoRepuestos from './ContenidoRepuestos/ContenidoRepuestos'
-// import Buscador from '/Buscador/Buscador'
 
 
 const Repuestos = () => {
     return (
         <div>
         <Header/>
-        {/* <Buscador/> */}
         <ContenidoRepuestos/>
         <ItemListContainer/>
         <Footer/>
